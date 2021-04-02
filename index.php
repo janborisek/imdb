682ec1d3f5a0276a9342edfe502a2ea3c5c09212
Jan Borišek <jan.borisek00@gmail.com>
2021-04-02 08:56:01 +0200

    drugi commit

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
         <title>Vaja Php</title>
     </head>
     <body>
-        <h1>Pozdravljeni</h1>
-        <h3>VSŠ je najjača višja šola na svetu, vesolju...</h3>
+        <h1>Dobrodošli na strani!!!</h1>
 
         <?php
             
