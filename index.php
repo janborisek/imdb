bbc8abaf0ea6a1138fb63b0857e35504d7193259
Jan Borišek <jan.borisek00@gmail.com>
2021-04-02 08:52:15 +0200

    prvi commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Vaja Php</title>
+    </head>
+    <body>
+        <h1>Pozdravljeni</h1>
+        <h3>VSŠ je najjača višja šola na svetu, vesolju...</h3>
+
+        <?php
+            
+            
+
+        ?>
+
+    </body>
+</html>
